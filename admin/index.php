65740b5f5429667830e8e064237390d6b21a44cc
sampurnathapa <90536713+sampurnathapa@users.noreply.github.com>
2022-08-20 02:04:37 +1000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,31 @@
+<?php
+	include('include/header.php');
+	include('php/functions.php');
+?>
 
+				<div id="page-wrapper">
+					<div class="header">
+					  <h1 class="page-header">
+					      Dashboard <small><?php echo $_SESSION['username'];?></small>
+					  </h1>
+					</div>
+
+					<div id="page-inner">
+
+						<?php include('include/inner-index.php');?>
+                        
+
+						<?php //include('include/index-page-inner.php');?>
+
+						
+
+					</div>
+					<!-- /. PAGE INNER  -->
+<div data-tockify-component="mini" data-tockify-calendar="spirited"></div>
+<script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
+				</div>
+			<!-- /. PAGE WRAPPER  -->
+<footer>
+					    <p>Copyright Pals and Partners. All Rights Reserved</a></p>
+					  </footer>
+<?php include('include/footer.php');?>
