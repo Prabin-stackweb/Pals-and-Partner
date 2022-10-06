8807ae7725240fd535aabd360f5ebac0f9ae399d
Sampurna Thapa Magar <90536713+sampurnathapa@users.noreply.github.com>
2022-10-06 12:26:14 +1100

    Update user_details.php

--- admin/php/user_details.php
+++ admin/php/user_details.php
@@ -1,5 +1,5 @@
 <?php
-    //database connection
+    //database connection//
     $mysqli= new mysqli("localhost","root", "", "pals");
     if (mysqli_connect_error()){
         printf("Connect failed: %s\n", mysqli_connect_error());
